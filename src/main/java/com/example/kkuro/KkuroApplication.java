d957e9d9ae1149f47986a3ab019cdf9215b52134
sim <simyechan87@gmail.com>
2023-09-08 09:23:17 +0900

    Initial commit

---
+++ src/main/java/com/example/kkuro/KkuroApplication.java
@@ -0,0 +1,13 @@
+package com.example.kkuro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkuroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkuroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kkuro/KkuroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kkuro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkuroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
